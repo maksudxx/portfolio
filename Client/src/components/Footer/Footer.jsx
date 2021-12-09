495948f9734aad1c2c0a9bdd9ef414abd90a764c
facundomaksud@gmail.com <facundomaksud@gmail.com>
2021-12-09 15:42:51 -0300

    Estilos footer modificados

--- Client/src/components/Footer/Footer.jsx
+++ Client/src/components/Footer/Footer.jsx
@@ -29,7 +29,8 @@ const useStyles = makeStyles((theme) => ({
   footer: {
     padding: theme.spacing(3, 2),
     marginTop: 'auto',
-    backgroundColor: "#FF7F60"
+    borderTop: '2px solid',
+    backgroundColor: "#FAFAFA"
       //theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
   },
 }));
