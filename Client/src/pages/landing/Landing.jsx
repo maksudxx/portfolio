e847db86fcbdf36a0c8332ad54c4be1f9a0c524d
facundomaksud@gmail.com <facundomaksud@gmail.com>
2021-12-13 13:10:23 -0300

    Estilos Landing Modificados

--- Client/src/pages/landing/Landing.jsx
+++ Client/src/pages/landing/Landing.jsx
@@ -4,14 +4,15 @@ import styles from "./Landing.module.css";
 export default function LandingPage() {
   return (
     <div className={styles.container}>
+      <div className={styles.containerTitle}>
       <div className={styles.title}>
         <p className={styles.parrafo}>FACUNDO MAKSUD</p>
         <p>FULL STACK DEVELOPER</p>
       </div>
-      <br />
       <Link to="/home" className={styles.link}>
         <button  className={styles.boton}>INICIO</button>
-      </Link>
+        </Link>
+      </div>
     </div>
   );
 }
