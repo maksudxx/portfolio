6298dc857570776b6e3cd1ba0006c57af524037b
facundomaksud@gmail.com <facundomaksud@gmail.com>
2021-12-15 13:47:06 -0300

    comentarios agregados

--- Client/src/pages/Contact/Contact.jsx
+++ Client/src/pages/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import styles from "./Contact.module.css";
 import { useForm } from "react-hook-form";
 import React, { useState } from "react";
 import { init, sendForm } from "emailjs-com";
-init("user_6ycfs4IYOD5RxbBK55C6l");
+init("user_6ycfs4IYOD5RxbBK55C6l");  //id de usuario que provee mailJs
 
 export default function Contact() {
   const { register, handleSubmit, watch, errors } = useForm();
@@ -18,9 +18,9 @@ export default function Contact() {
   };
   const onSubmit = (data) => {
     //console.log(data);
-    generateContactNumber();
-    sendForm("default_service", "template_a7axzva", "#contact-form").then(
-      function (response) {
+    generateContactNumber(); 
+    sendForm("default_service", "template_a7axzva", "#contact-form").then(  //agregamos el numero de template que se genera cuando lo creamos desde la pagina
+      function (response) { 
         console.log("SUCCESS!", response.status, response.text);
       },
       function (error) {
@@ -46,6 +46,8 @@ export default function Contact() {
         <Networks />
       </div>
       <br />
+
+      {/* en el formulario la propiedad name de los input tienen que coincidir con los del template para que se envie el mje correctamente */}
       <form id="contact-form" onSubmit={handleSubmit(onSubmit)}>
         <p>Dejar Mensaje: </p>
         <input
